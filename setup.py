e767b72a19ff3f6d8dfd54840df8e24c97475819
Ryan Belgrave <rbelgrave@covermymeds.com>
2015-08-11 14:50:42 -0400

    add repo URL, fix author and remove email

--- setup.py
+++ setup.py
@@ -3,19 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name='hq-framework',
     version='1.0.0',
-    url='',
+    url='https://github.com/herqles-io/hq-framework',
     include_package_data=True,
-    license='',
-    author='Ryan Belgrave',
-    author_email='rbelgrave@covermymeds.com',
+    license='MIT',
+    author='CoverMyMeds',
     description='Herqles Framework',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     install_requires=[
         'hq-lib==1.0.0',
         'pika==0.9.14',
-        'cherrypy==3.6.0',
-        'schematics==1.0.4'
+        'cherrypy==3.8.0',
+        'schematics==1.1.0'
     ],
     dependency_links=[
         'git+https://github.com/herqles-io/hq-lib.git#egg=hq-lib-1.0.0'
