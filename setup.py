ca00bd0cf7f1cfcac4eaffe9ac0298e775729987
Jodie Belgrave <rmb1993@gmail.com>
2015-09-27 14:57:46 -0400

    fix development build versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hq-framework',
-    version='2.0.0-dev',
+    version='2.0.0.dev1',
     url='https://github.com/herqles-io/hq-framework',
     include_package_data=True,
     license='MIT',
@@ -11,13 +11,10 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages('src'),
     install_requires=[
-        'hq-lib==1.0.0',
+        'hq-lib==2.0.0.dev1',
         'pika==0.9.14',
         'cherrypy==3.8.0',
         'schematics==1.1.0'
-    ],
-    dependency_links=[
-        'git+https://github.com/herqles-io/hq-lib.git@2.0-dev#egg=hq-lib-2.0.0-dev'
     ],
     scripts=['bin/hq-framework']
 )
